1f6c988e36f17aea4e23a91868bc45d3107f888e
piratecodes <subhamsarkar1999@gmail.com>
2026-04-23 03:13:10 +0530

    23-04-2026 minor changes

--- src/componant/footer.jsx
+++ src/componant/footer.jsx
@@ -97,7 +97,6 @@ export default function Footer() {
               <h4 className="text-white font-semibold mb-2">Quick Links</h4>
               <ul className="flex flex-col gap-3 text-sm">
                 <li><Link href="/" className="hover:text-secondary transition-colors">Home</Link></li>
-                <li><a target='_blank' href="https://blog.pradhanservice.com" className="hover:text-secondary transition-colors">Blogs</a></li>
                 <li><Link href="/photo-gallery" className="hover:text-secondary transition-colors">Gallery</Link></li>
                 <li><Link href="/about" className="hover:text-secondary transition-colors">About Us</Link></li>
                 <li><Link href="/contact" className="hover:text-secondary transition-colors">Contact Us</Link></li>
